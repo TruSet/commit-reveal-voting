2d562ceca43608318f7b94c545a1ebff1393cb7e
Neil McLaren <neil.mclaren@consensys.net>
2018-08-29 11:57:25 +0100

    Allow vote revelation by non-voters, and add some tests.

--- test/commitRevealVoting.js
+++ test/commitRevealVoting.js
@@ -19,7 +19,13 @@ contract('CommitRevealVoting', function (accounts) {
 
   it('creates a poll', async function () {
     let pollID = 'testPoll'
+
+    let pollExists = await crv.pollExists(pollID)
+    assert.equal(pollExists, false, 'poll does not exist before creation')
+
     await crv.startPoll(pollID, 24*60*60, 5*60*60)
+    pollExists = await crv.pollExists(pollID)
+    assert.equal(pollExists, true, 'poll exists after creation')
 
     let pollEnded = await crv.pollEnded.call(pollID)
     assert.equal(pollEnded, false, 'poll should be open')
@@ -42,7 +48,7 @@ contract('CommitRevealVoting', function (accounts) {
     })
 
     it('allows you to commit a vote', async function() {
-      let secretVote = utils.createVoteHash('1', defaultSalt)
+      let secretVote = utils.createVoteHash('0', defaultSalt)
       await crv.commitVote(pollID, secretVote)
 
       let voteCounts = await crv.getVoteCounts.call(pollID)
@@ -52,25 +58,61 @@ contract('CommitRevealVoting', function (accounts) {
       assert.equal(didCommit, true, 'user should be able to commit a vote')
     })
 
-    it('allows you to reveal your vote', async function() {
+    it('allows you to reveal your own vote', async function() {
       let secretVote = utils.createVoteHash('1', defaultSalt)
       await crv.commitVote(pollID, secretVote, { from: neil })
 
+      let revealPeriodActive = await crv.revealPeriodActive.call(pollID)
+      assert.equal(revealPeriodActive, false, 'poll should not be in the reveal period yet')
+      let commitPeriodActive = await crv.commitPeriodActive.call(pollID)
+      assert.equal(commitPeriodActive, true, 'poll should be in the commit period')
+
       await web3.currentProvider.send({jsonrpc: "2.0", method: "evm_increaseTime", params: [25*3600], id: 0})
 
-      let revealPeriodActive = await crv.revealPeriodActive.call(pollID)
+      revealPeriodActive = await crv.revealPeriodActive.call(pollID)
       assert.equal(revealPeriodActive, true, 'poll should enter the reveal period after 25 hours')
+      commitPeriodActive = await crv.commitPeriodActive.call(pollID)
+      assert.equal(commitPeriodActive, false, 'poll should no longer be in the commit period')
 
       let voteCounts = await crv.getVoteCounts.call(pollID)
       expect(voteCounts.map(e => e.toNumber())).to.deep.equal([0,0,2])
 
-      await crv.revealVote(pollID, '1', defaultSalt, { from: neil })
+      await crv.revealMyVote(pollID, '1', defaultSalt, { from: neil })
 
       voteCounts = await crv.getVoteCounts.call(pollID)
       expect(voteCounts.map(e => e.toNumber())).to.deep.equal([1,0,1])
 
       let didReveal = await crv.didReveal.call(pollID, neil)
       assert.equal(didReveal, true, 'user should be able to reveal a vote')
+
+      let votedFor = await crv.didVoteFor.call(pollID, neil)
+      assert.equal(votedFor, true, '\'user voted for\' tacked as expected')
+
+      let votedAgainst = await crv.didVoteAgainst.call(pollID, neil)
+      assert.equal(votedAgainst, false, '\'user voted against\' tacked as expected')
     })
+
+    it('allows anyone to reveal someone else\'s vote', async function() {
+      let voteCounts = await crv.getVoteCounts.call(pollID)
+      expect(voteCounts.map(e => e.toNumber())).to.deep.equal([1,0,1])
+
+      await crv.revealVote(pollID, greg, 0, defaultSalt, { from: neil })
+
+      voteCounts = await crv.getVoteCounts.call(pollID)
+      expect(voteCounts.map(e => e.toNumber())).to.deep.equal([1,1,0])
+
+      let didReveal = await crv.didReveal.call(pollID, greg)
+      assert.equal(didReveal, true, 'greg\'s vote was revealed')
+
+      let votedFor = await crv.didVoteFor.call(pollID, greg)
+      assert.equal(votedFor, false, '\'user voted for\' tacked as expected')
+
+      let votedAgainst = await crv.didVoteAgainst.call(pollID, greg)
+      assert.equal(votedAgainst, true, '\'user voted against\' tacked as expected')
+    })
+
+    // Tests TODO: committing multiple votes
+    //             revealing multiple votes
+    //             checking 
   })
 })
