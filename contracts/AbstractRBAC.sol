24384755b6fa5cdcad66e6f570b36736be23a190
Neil McLaren <neil.mclaren@consensys.net>
2019-07-16 15:11:38 +0100

    Reinstate a test RBAC (now standalone rather than based on OpenZeppelin or TruSetRBAC) so that migrations run.

--- contracts/AbstractRBAC.sol
+++ contracts/AbstractRBAC.sol
@@ -1,5 +1,6 @@
 pragma solidity 0.5.10;
 
 contract AbstractRBAC {
-  function checkRole(address _operator, string memory _role) view public;
+  function checkAdmin(address _user) view public;
+  function checkVoter(address _user) view public;
 }
